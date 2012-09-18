2f8590bc9ff5ffa4691cd85468d9cf8837c55caa
Andrey Balakirev <balakirev.andrey@gmail.com>
2012-09-18 18:06:10 +0400

    страница статей

--- static/js/hiding_list.js
+++ static/js/hiding_list.js
@@ -12,6 +12,7 @@ $(function(){
   function check(){
     var hide_menu = el.scrollTop() > top_offset + ( menu_shown ? top_dif : 0 );
     var height    = 0;
+    var changes   = false;
 
     if ( hide_menu && menu_shown ) {
       menu_shown = false;
@@ -19,6 +20,7 @@ $(function(){
       menu.hide();
       desc.attr( 'class', 'span9' );
       if ( !top_dif ) top_dif = height - el.height();
+      changes = true;
     }
 
     if ( !hide_menu && !menu_shown ) {
@@ -27,6 +29,11 @@ $(function(){
       menu.show();
       desc.attr( 'class', 'span6' );
       if ( !top_dif ) top_dif = el.height() - height;
+      changes = true;
     }
+
+    //todo: переделать
+    if ( changes )
+      setTimeout( function(){ $('body').scrollspy('refresh') }, 100 );
   }
 })
